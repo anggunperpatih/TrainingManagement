1e9a4af67e1fdeeac10dcb5b9960f01ac98499a1
anggunperpatih <sidi.anggunperpatih@gmail.com>
2026-08-08 13:12:38 +0700

    Add scoped reporting, admin assignment audit, and security tests

--- src/OperationsRequests/obj/Release/net10.0/OperationsRequests.AssemblyInfo.cs
+++ src/OperationsRequests/obj/Release/net10.0/OperationsRequests.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperationsRequests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8caf75cf34137460d19941befe192143d3e46a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperationsRequests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperationsRequests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
